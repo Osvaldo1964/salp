c2baa4087a001f976251cae964df021bdf13cf48
Osvaldo1964 <140724795+Osvaldo1964@users.noreply.github.com>
2024-08-04 17:10:45 -0500

    table_002

--- db-salp.sql
+++ db-salp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-08-2024 a las 18:28:35
+-- Tiempo de generación: 05-08-2024 a las 00:10:16
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -24,6 +24,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `classes`
+--
+
+CREATE TABLE `classes` (
+  `id_class` bigint(20) NOT NULL,
+  `name_class` text NOT NULL,
+  `life_class` decimal(10,2) NOT NULL,
+  `status_class` varchar(8) NOT NULL DEFAULT 'Activo',
+  `date_created_class` date DEFAULT NULL,
+  `date_updated_class` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `classes`
+--
+
+INSERT INTO `classes` (`id_class`, `name_class`, `life_class`, `status_class`, `date_created_class`, `date_updated_class`) VALUES
+(1, 'LUMINARIAS', 3.50, 'Activo', '2024-08-04', '2024-08-04 17:36:23');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `concepts`
 --
 
@@ -37,6 +59,32 @@ CREATE TABLE `concepts` (
   `date_created_concept` date DEFAULT NULL,
   `date_updated_concept` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `crews`
+--
+
+CREATE TABLE `crews` (
+  `id_crew` bigint(20) NOT NULL,
+  `name_crew` text NOT NULL,
+  `driver_crew` text NOT NULL,
+  `tecno_crew` text NOT NULL,
+  `assist_crew` text NOT NULL,
+  `status_crew` varchar(8) NOT NULL DEFAULT 'Activo',
+  `date_created_crew` date DEFAULT NULL,
+  `date_updated_crew` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `crews`
+--
+
+INSERT INTO `crews` (`id_crew`, `name_crew`, `driver_crew`, `tecno_crew`, `assist_crew`, `status_crew`, `date_created_crew`, `date_updated_crew`) VALUES
+(1, 'CUADRILLA BASE', 'JUAN', 'PEDRO', 'JACOBO', 'Activo', '2024-08-03', '2024-08-03 21:09:01'),
+(2, 'CUADRILLA PESADA', 'ALBERTO', 'SANTIAGO', 'RENE', 'Activo', '2024-08-03', '2024-08-03 21:09:01'),
+(3, 'OTRA CUADRILLA PESADA A', 'MARIA B F', 'CAMILA C FG', 'SOFIA D', 'Activo', '2024-08-04', '2024-08-03 23:29:23');
 
 -- --------------------------------------------------------
 
@@ -61,6 +109,49 @@ CREATE TABLE `deliveries` (
 
 INSERT INTO `deliveries` (`id_delivery`, `id_typedelivery_delivery`, `id_itemdelivery_delivery`, `number_delivery`, `date_delivery`, `id_resources_delivery`, `date_created_delivery`, `date_updated_delivery`) VALUES
 (1, 1, 1, 'INI-001', '2024-08-01', 2, '2024-08-02', '2024-08-02 16:18:38');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `elements`
+--
+
+CREATE TABLE `elements` (
+  `id_element` bigint(20) NOT NULL,
+  `class_element` bigint(20) NOT NULL,
+  `code_element` varchar(15) NOT NULL,
+  `name_element` text NOT NULL,
+  `events_element` text NOT NULL,
+  `address_element` text NOT NULL,
+  `id_minute_element` bigint(20) NOT NULL,
+  `id_resource_element` bigint(20) NOT NULL,
+  `id_use_element` bigint(20) NOT NULL,
+  `id_tecnology_element` bigint(20) NOT NULL,
+  `id_power_element` bigint(20) NOT NULL,
+  `id_material_element` bigint(20) NOT NULL,
+  `altitud_element` float NOT NULL,
+  `latitud_element` float NOT NULL,
+  `longitud_element` float NOT NULL,
+  `id_dispose_element` bigint(20) NOT NULL,
+  `value_element` decimal(15,2) NOT NULL,
+  `status_elelemt` varchar(8) NOT NULL,
+  `date_created_element` date DEFAULT NULL,
+  `date_updated_element` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `heights`
+--
+
+CREATE TABLE `heights` (
+  `id_height` bigint(20) NOT NULL,
+  `name_height` text NOT NULL,
+  `status_height` varchar(8) NOT NULL DEFAULT 'Activo',
+  `date_created_height` date DEFAULT NULL,
+  `date_updated_height` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -90,6 +181,51 @@ INSERT INTO `itemdeliveries` (`id_itemdelivery`, `code_itemdelivery`, `id_typede
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `powers`
+--
+
+CREATE TABLE `powers` (
+  `id_power` bigint(20) NOT NULL,
+  `name_power` text NOT NULL,
+  `status_power` varchar(8) NOT NULL DEFAULT 'Activo',
+  `date_created_power` date DEFAULT NULL,
+  `date_updated_power` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `powers`
+--
+
+INSERT INTO `powers` (`id_power`, `name_power`, `status_power`, `date_created_power`, `date_updated_power`) VALUES
+(1, '30W', 'Activo', '2024-08-04', '2024-08-04 15:46:17');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pqrs`
+--
+
+CREATE TABLE `pqrs` (
+  `id_pqr` bigint(20) NOT NULL,
+  `name_pqr` text NOT NULL,
+  `email_pqr` text NOT NULL,
+  `address_pqr` text NOT NULL,
+  `message_pqr` text NOT NULL,
+  `id_element_pqr` bigint(20) NOT NULL,
+  `date_asign_pqr` date DEFAULT NULL,
+  `id_creq_pqr` bigint(20) NOT NULL,
+  `date_solution_pqr` date DEFAULT NULL,
+  `latitud_pqr` float NOT NULL,
+  `longitud_pqr` float NOT NULL,
+  `name_address_pqr` text NOT NULL,
+  `status_pqr` varchar(8) NOT NULL,
+  `date_created_pqr` date DEFAULT NULL,
+  `date_updated_pqr` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `resources`
 --
 
@@ -108,6 +244,42 @@ CREATE TABLE `resources` (
 INSERT INTO `resources` (`id_resource`, `name_resource`, `status_resource`, `date_created_resource`, `date_updated_resource`) VALUES
 (1, 'INVERIONISTA', 'Activo', '2024-08-02', '2024-08-02 16:20:18'),
 (2, 'RECURSOS PROPIOS', 'Activo', '2024-08-02', '2024-08-02 16:20:18');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rouds`
+--
+
+CREATE TABLE `rouds` (
+  `id_roud` bigint(20) NOT NULL,
+  `code_roud` varchar(4) NOT NULL,
+  `name_roud` text NOT NULL,
+  `status_roud` varchar(8) NOT NULL DEFAULT 'Activo',
+  `date_created_roud` date DEFAULT NULL,
+  `date_updated_roud` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `rouds`
+--
+
+INSERT INTO `rouds` (`id_roud`, `code_roud`, `name_roud`, `status_roud`, `date_created_roud`, `date_updated_roud`) VALUES
+(1, 'M1', 'AUTOPISTAS Y CARRETERAS', 'Activo', '2024-08-05', '2024-08-04 22:02:32');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `technologies`
+--
+
+CREATE TABLE `technologies` (
+  `id_technology` bigint(20) DEFAULT NULL,
+  `name_technology` text NOT NULL,
+  `status_technology` varchar(8) NOT NULL DEFAULT 'Activo',
+  `date_created_technology` date DEFAULT NULL,
+  `date_updated_technology` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -164,7 +336,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id_user`, `fullname_user`, `username_user`, `email_user`, `password_user`, `token_user`, `token_exp_user`, `id_rol_user`, `picture_user`, `country_user`, `city_user`, `address_user`, `phone_user`, `method_user`, `date_created_user`, `date_updated_user`, `status_user`, `verification_user`) VALUES
-(1, 'Osvaldo José Villalobos Cortina', 'osvicor', 'osvicor@hotmail.com', '$2a$07$azybxcags23425sdg23sdeanQZqjaf6Birm2NvcYTNtJw24CsO5uq', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MjI2MTQ0NDEsImV4cCI6MTcyMjcwMDg0MSwiZGF0YSI6eyJpZCI6MSwiZW1haWwiOiJvc3ZpY29yQGhvdG1haWwuY29tIn19.h3nW5UGOrll34Z8yzYyIOtaVQqEfYmgj_dMXmdvmg1I', '1722700841', 'Administradores', '1.jpg', 'Afghanistan', 'Santa Marta', 'Urb. San Lorenzo Mz J Cs 34', '93_3153153153', 'direct', '2024-06-17', '2024-06-17 18:47:27', 1, 0),
+(1, 'Osvaldo José Villalobos Cortina', 'osvicor', 'osvicor@hotmail.com', '$2a$07$azybxcags23425sdg23sdeanQZqjaf6Birm2NvcYTNtJw24CsO5uq', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MjI4MDkxMDYsImV4cCI6MTcyMjg5NTUwNiwiZGF0YSI6eyJpZCI6MSwiZW1haWwiOiJvc3ZpY29yQGhvdG1haWwuY29tIn19.-Xkb9LJ2E2mjxm6EXSwjeqEBUHwR9av3_kcZgCD2Hwk', '1722895506', 'Administradores', '1.jpg', 'Afghanistan', 'Santa Marta', 'Urb. San Lorenzo Mz J Cs 34', '93_3153153153', 'direct', '2024-06-17', '2024-06-17 18:47:27', 1, 0),
 (7, 'Jorge Villalobos', 'jorgito', 'jorge@gmail.com', '$2a$07$azybxcags23425sdg23sdeanQZqjaf6Birm2NvcYTNtJw24CsO5uq', NULL, NULL, 'Usuarios', '7.png', 'Algeria', 'Varsobia', 'LA QUE SEA', '+213_3153153153', 'direct', '2024-06-22', '2024-06-22 14:18:30', 1, 1),
 (31, 'Juan Prueto', '', 'prueba@mail.com', '', NULL, NULL, '1', NULL, 'Afghanistan', 'Otra', 'calle 1', '+93_3253253325', 'direct', '2024-06-25', '2024-06-25 20:10:46', 1, 1);
 
@@ -173,10 +345,22 @@ INSERT INTO `users` (`id_user`, `fullname_user`, `username_user`, `email_user`, 
 --
 
 --
+-- Indices de la tabla `classes`
+--
+ALTER TABLE `classes`
+  ADD PRIMARY KEY (`id_class`);
+
+--
 -- Indices de la tabla `concepts`
 --
 ALTER TABLE `concepts`
   ADD PRIMARY KEY (`id_concept`);
+
+--
+-- Indices de la tabla `crews`
+--
+ALTER TABLE `crews`
+  ADD PRIMARY KEY (`id_crew`);
 
 --
 -- Indices de la tabla `deliveries`
@@ -185,16 +369,46 @@ ALTER TABLE `deliveries`
   ADD PRIMARY KEY (`id_delivery`);
 
 --
+-- Indices de la tabla `elements`
+--
+ALTER TABLE `elements`
+  ADD PRIMARY KEY (`id_element`);
+
+--
+-- Indices de la tabla `heights`
+--
+ALTER TABLE `heights`
+  ADD PRIMARY KEY (`id_height`);
+
+--
 -- Indices de la tabla `itemdeliveries`
 --
 ALTER TABLE `itemdeliveries`
   ADD PRIMARY KEY (`id_itemdelivery`);
 
 --
+-- Indices de la tabla `powers`
+--
+ALTER TABLE `powers`
+  ADD PRIMARY KEY (`id_power`);
+
+--
+-- Indices de la tabla `pqrs`
+--
+ALTER TABLE `pqrs`
+  ADD PRIMARY KEY (`id_pqr`);
+
+--
 -- Indices de la tabla `resources`
 --
 ALTER TABLE `resources`
   ADD PRIMARY KEY (`id_resource`);
+
+--
+-- Indices de la tabla `rouds`
+--
+ALTER TABLE `rouds`
+  ADD PRIMARY KEY (`id_roud`);
 
 --
 -- Indices de la tabla `typedeliveries`
@@ -214,10 +428,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `classes`
+--
+ALTER TABLE `classes`
+  MODIFY `id_class` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT de la tabla `concepts`
 --
 ALTER TABLE `concepts`
   MODIFY `id_concept` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `crews`
+--
+ALTER TABLE `crews`
+  MODIFY `id_crew` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `deliveries`
@@ -226,16 +452,46 @@ ALTER TABLE `deliveries`
   MODIFY `id_delivery` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT de la tabla `elements`
+--
+ALTER TABLE `elements`
+  MODIFY `id_element` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `heights`
+--
+ALTER TABLE `heights`
+  MODIFY `id_height` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `itemdeliveries`
 --
 ALTER TABLE `itemdeliveries`
   MODIFY `id_itemdelivery` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT de la tabla `powers`
+--
+ALTER TABLE `powers`
+  MODIFY `id_power` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `pqrs`
+--
+ALTER TABLE `pqrs`
+  MODIFY `id_pqr` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `resources`
 --
 ALTER TABLE `resources`
-  MODIFY `id_resource` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_resource` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `rouds`
+--
+ALTER TABLE `rouds`
+  MODIFY `id_roud` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `typedeliveries`
